51d6a4ac4bb405c6800e2e46ce61dbcf5f1718e2
msi1427 <sabikirbaz@iut-dhaka.edu>
2022-12-14 18:43:13 +0600

    selenium scraper and tableau public view added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Demographics-of-Best-CS-Scientists-Worldwide
+Tableau Public View: https://public.tableau.com/app/profile/mohammad.sabik.s.irbaz/viz/DemographicsofBestCSScientistsWorldwide/Dashboard1
---
+++ selenium_scraper/scraper.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+columns = ["World Rank", "National Rank", "Name", "Image URLs", "Affiliation", "Country", "H-Index", "Citations", "#DBLP"]
+
+def get_scholar_details(row):
+    details = row.text.split('\n')
+    contents = {}
+    contents["World Rank"] =    details[0]
+    contents["National Rank"] = details[1]
+    contents["Name"] =          details[2]
+    contents["Affiliation"] =   details[3].split(',')[0]
+    contents["Country"] =       details[3].split(',')[1].strip()
+    contents["H-Index"] =       details[4]
+    contents["Citations"] =     details[5].replace(',','')
+    contents["#DBLP"] =         details[6].replace(',','')
+    contents["Image URLs"] =    row.find_element(By.CLASS_NAME, 'lazyload').get_attribute('src')
+    return contents
+
+
+def main():
+    webdriver_path = "C:\Program Files (x86)\chromedriver.exe"
+    scholar_data = []
+
+    for page_id in range(1,11): 
+        driver = webdriver.Chrome(webdriver_path)
+        url = f"https://research.com/scientists-rankings/computer-science?page={page_id}"
+        driver.get(url)
+        rankings = driver.find_element(By.ID, 'rankingItems')
+        rows = rankings.find_elements(By.CLASS_NAME, 'cols')
+        for idx, row in enumerate(rows):
+            if idx % 4 == 0:
+                scholar_data.append(get_scholar_details(row))
+        driver.close()
+
+    df = pd.DataFrame(data=scholar_data, columns=columns)
+    df.to_csv("best_cs_scientist_details.csv", index=False)
+    return
+
+if __name__ == "__main__":
+    main()
